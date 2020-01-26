6279d24e1e73e370fb6a139797eb6aaf3c5bd728
thekodinator <60329768+thekodinator@users.noreply.github.com>
2020-01-26 18:10:26 -0500

    Update TODOS in app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("/mxport-a2a07-firebase-adminsdk-11xla-873cf3ecd4.json");
 
 // TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "https://mxport-a2a07.firebaseio.com/"
+});
 
 app.set('port', (process.env.PORT || 3001));
 
